9c048eb2d34d4b99b7e027330da221c47a1adb07
creative301 <rza9001@gmail.com>
2019-03-09 22:37:34 +0700

    Update uml diagram

--- Project_4/express_test.sql
+++ Project_4/express_test.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 05, 2019 at 12:53 AM
+-- Host: localhost
+-- Generation Time: Mar 09, 2019 at 11:19 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
@@ -34,6 +34,17 @@ CREATE TABLE `chef` (
   `last_name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `chef`
+--
+
+INSERT INTO `chef` (`chef_id`, `first_name`, `last_name`) VALUES
+(1, 'Gordon ', 'Ramsay'),
+(2, 'Jamie ', 'Oliver'),
+(3, 'Rachel', 'Ray'),
+(4, 'Bobby', 'Flay'),
+(5, 'Emeril ', 'Lagasse');
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +61,13 @@ CREATE TABLE `customer` (
   `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `order_id`, `first_name`, `last_name`, `address`, `phone`, `email`) VALUES
+(2, 1, 'David', 'Jones', '5488 W 34th St', '5551234567', 'david@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +83,13 @@ CREATE TABLE `delivery_person` (
   `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `delivery_person`
+--
+
+INSERT INTO `delivery_person` (`delivery_person_id`, `order_id`, `first_name`, `last_name`, `phone`, `email`) VALUES
+(2, 1, 'Christopher', 'Layton', '5557654321', 'chris@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +104,14 @@ CREATE TABLE `menu` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`menu_id`, `menu_item_id`, `main_dishes`, `desserts`, `date`) VALUES
+(1, 1, 'Spicy Honey-Brushed Chicken Thighs', 'Classic Crème Brûlée', '2019-03-04'),
+(2, 2, 'Easy Baked Fish Fillets', 'Flourless Chocolate Espresso Cake', '2019-03-04');
+
 -- --------------------------------------------------------
 
 --
@@ -89,8 +122,16 @@ CREATE TABLE `menu_item` (
   `menu_item_id` int(11) NOT NULL,
   `menu_id` int(11) NOT NULL,
   `restaurant_id` int(11) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `menu_item`
+--
+
+INSERT INTO `menu_item` (`menu_item_id`, `menu_id`, `restaurant_id`, `price`, `name`) VALUES
+(1, 1, 1, 250, '');
 
 -- --------------------------------------------------------
 
@@ -101,8 +142,15 @@ CREATE TABLE `menu_item` (
 CREATE TABLE `order_detail` (
   `order_id` int(11) NOT NULL,
   `menu_item_id` int(11) NOT NULL,
-  `quantity` tinyint(4) NOT NULL
+  `quantity` smallint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`order_id`, `menu_item_id`, `quantity`) VALUES
+(1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -114,11 +162,18 @@ CREATE TABLE `order_history` (
   `order_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `delivery_person_id` int(11) NOT NULL,
-  `priceTotal` tinyint(4) NOT NULL,
+  `priceTotal` mediumint(4) NOT NULL,
   `date` date NOT NULL,
   `order_time` time NOT NULL,
   `delivery_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_history`
+--
+
+INSERT INTO `order_history` (`order_id`, `customer_id`, `delivery_person_id`, `priceTotal`, `date`, `order_time`, `delivery_time`) VALUES
+(1, 1, 1, 500, '2019-03-04', '12:00:00', '12:10:00');
 
 -- --------------------------------------------------------
 
@@ -131,8 +186,15 @@ CREATE TABLE `restaurant` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`restaurant_id`, `name`, `address`, `phone`, `email`) VALUES
+(1, 'Express Food', 'Menlo Park', '833272077', 'expressfood@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -199,43 +261,43 @@ ALTER TABLE `restaurant`
 -- AUTO_INCREMENT for table `chef`
 --
 ALTER TABLE `chef`
-  MODIFY `chef_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `chef_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `delivery_person`
 --
 ALTER TABLE `delivery_person`
-  MODIFY `delivery_person_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `delivery_person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `menu_item`
 --
 ALTER TABLE `menu_item`
-  MODIFY `menu_item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `menu_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `order_history`
 --
 ALTER TABLE `order_history`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables

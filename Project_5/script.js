3207c91d6626fb72f181ef3d1915aaa5a2947991
creative301 <rza9001@gmail.com>
2019-03-25 17:06:27 +0700

    Update project 5 script.js

--- Project_5/script.js
+++ Project_5/script.js
@@ -22,9 +22,9 @@ function displayMotivationalQuote() {
 
 
 // Work in progress
-// Step 2
+// Step 2 original
 
-let quoteNumber, quoteType;
+/* let quoteNumber, quoteType;
 let quotes = []
 
 const motivationalQuoteText = {
@@ -83,4 +83,75 @@ function createQuote () {
     inspirationalQuote = `${motivationalQuoteText.beginningQuote[beginningMotivationalIndex]} ${motivationalQuoteText.middleQuote[middleMotivationalIndex]} ${motivationalQuoteText.endQuote[endMotivationalIndex]}`;
 
     return motivationalQuote;
+} */
+
+
+// Step 2 edited
+
+let quoteNumber, quoteType;
+let quotes = [];
+
+runQuoteNumber();
+
+function runQuoteNumber() {
+    quoteNumber = parseInt(prompt('Please input the number of quote (1-5)')); 
+}
+
+runQuoteType();
+
+function runQuoteType() {
+    quoteType = parseInt(prompt('Please input the quote type (1 = Motivational or 2 = Inspirational)')); 
+}
+
+chooseQuoteType();
+
+function chooseQuoteType() {
+    if (quoteType === 1) {
+        for (let i = 0; i < quoteNumber; i++) {
+            console.log(displayMotivationalQuote());       
+        }
+    } else {
+        for (let i = 0; i < quoteNumber; i++) {
+            console.log(displayInspirationalQuote());     
+        }
+    }
+}
+
+// Function to generate random motivational quote
+function displayMotivationalQuote() {
+    let motivationalQuote, beginningMotivationalIndex, middleMotivationalIndex, endMotivationalIndex;
+
+    const motivationalQuoteText = {
+        beginningQuote: ["Whether you think you can", "The future is now", "Be who you are and say what you feel", "You only live once", "Do what you can"],
+        middleQuote: ["or whether you think you can't", "it's time to grow up and be strong", "because those who mind don't matter", "but if you do it right", "with what you have"],
+        endQuote: ["you're right", "tomorrow may well be too late", "and those who matter don't mind", "once is enough", "where you are"]
+    }
+
+    beginningMotivationalIndex = Math.floor(Math.random() * motivationalQuoteText.beginningQuote.length);
+    middleMotivationalIndex = Math.floor(Math.random() * motivationalQuoteText.middleQuote.length);
+    endMotivationalIndex = Math.floor(Math.random() * motivationalQuoteText.endQuote.length);
+
+    motivationalQuote = `${motivationalQuoteText.beginningQuote[beginningMotivationalIndex]} ${motivationalQuoteText.middleQuote[middleMotivationalIndex]} ${motivationalQuoteText.endQuote[endMotivationalIndex]}`;
+ 
+    return motivationalQuote; 
+
+}
+
+// Function to generate random inspirational quote
+function displayInspirationalQuote () {
+    let inspirationalQuote, beginningInspirationalIndex, middleInspirationalIndex, endInspirationalIndex;
+
+    const inspirationalQuoteText = {
+        beginningQuote : ["The way to get started", "Never stop dreaming", "Forget yesterday - it has already forgotten you", "You’re not obligated to win", "If you have a dream, don’t just sit there"],
+        middleQuote : ["is to quit talking", "never stop believing", "don't sweat tomorrow - you haven't even met", "you’re obligated to keep trying", "gather courage to believe that you can succeed"],
+        endQuote : ["and begin doing", "never give up", "open your eyes and your heart to a truly precious gift - today", "To the best you can do everyday", "and leave no stone unturned to make it a reality"]
+    }
+
+    beginningInspirationalIndex = Math.floor(Math.random() * inspirationalQuoteText.beginningQuote.length);
+    middleInspirationalIndex = Math.floor(Math.random() * inspirationalQuoteText.middleQuote.length);
+    endInspirationalIndex = Math.floor(Math.random() * inspirationalQuoteText.endQuote.length);
+
+    inspirationalQuote = `${inspirationalQuoteText.beginningQuote[beginningInspirationalIndex]} ${inspirationalQuoteText.middleQuote[middleInspirationalIndex]} ${inspirationalQuoteText.endQuote[endInspirationalIndex]}`;
+
+    return inspirationalQuote;       
 }
